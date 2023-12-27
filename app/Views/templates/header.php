532c5fbca0a2b30d565d4ce1abea9e458c93ab9d
moodangza <moodangza29@gmail.com>
2023-12-27 10:21:45 +0700

    te

---
+++ app/Views/templates/header.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Spica Admin</title>
+  <!-- base:css -->
+  <link rel="stylesheet" href="./firsttemplate/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="./firsttemplate/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="./firsttemplate/css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="./firsttemplate/images/favicon.png" />
+
+    <!-- base:js -->
+    <script src="./firsttemplate/vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <script src="./firsttemplate/vendors/chart.js/Chart.min.js"></script>
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="./firsttemplate/js/off-canvas.js"></script>
+  <script src="./firsttemplate/js/hoverable-collapse.js"></script>
+  <script src="./firsttemplate/js/template.js"></script>
+  <!-- endinject -->
+  <!-- plugin js for this page -->
+  <!-- End plugin js for this page -->
+  <!-- Custom js for this page-->
+  <script src="./firsttemplate/js/dashboard.js"></script>
+  <!-- End custom js for this page-->
+</head>
+<body>
+    <?= $this->renderSection('content') ?>
+    
+
+
+
+</body>
+</html>
