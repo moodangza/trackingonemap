608f4b1a8380bf96d5c39a72acc201d57898d9b2
moodangza <moodangza29@gmail.com>
2024-01-12 10:17:06 +0700

    Update index.php

--- app/Views/spica/index.php
+++ app/Views/spica/index.php
@@ -16,61 +16,21 @@
       <div class="main-panel">
         <div class="content-wrapper">
           <div class="row">
-        <?php   echo getcwd();;
-        
-        ?>
+        <?php   //echo getcwd();; ?>
             <div class="col-12 col-xl-6 grid-margin stretch-card">
               <div class="row w-100 flex-grow">
                 <div class="col-md-12 grid-margin stretch-card">
                   <div class="card">
                     <div class="card-body">
-                      <p class="card-title">Website Audience Metrics</p>
-                      <p class="text-muted">25% more traffic than previous week</p>
+                      <button class="btn btn-primary">เพิ่มหัวข้อ</button>
+                      
                       <div class="row mb-3">
                         <div class="col-md-7">
-                          <div class="d-flex justify-content-between traffic-status">
-                            <div class="item">
-                              <p class="mb-">Users</p>
-                              <h5 class="font-weight-bold mb-0">93,956</h5>
-                              <div class="color-border"></div>
-                            </div>
-                            <div class="item">
-                              <p class="mb-">Bounce Rate</p>
-                              <h5 class="font-weight-bold mb-0">58,605</h5>
-                              <div class="color-border"></div>
-                            </div>
-                            <div class="item">
-                              <p class="mb-">Page Views</p>
-                              <h5 class="font-weight-bold mb-0">78,254</h5>
-                              <div class="color-border"></div>
-                            </div>
-                          </div>
+                       
                         </div>
-                        <div class="col-md-5">
-                          <ul class="nav nav-pills nav-pills-custom justify-content-md-end" id="pills-tab-custom"
-                            role="tablist">
-                            <li class="nav-item">
-                              <a class="nav-link active" id="pills-home-tab-custom" data-toggle="pill"
-                                href="#pills-health" role="tab" aria-controls="pills-home" aria-selected="true">
-                                Day
-                              </a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" id="pills-profile-tab-custom" data-toggle="pill" href="#pills-career"
-                                role="tab" aria-controls="pills-profile" aria-selected="false">
-                                Week
-                              </a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" id="pills-contact-tab-custom" data-toggle="pill" href="#pills-music"
-                                role="tab" aria-controls="pills-contact" aria-selected="false">
-                                Month
-                              </a>
-                            </li>
-                          </ul>
-                        </div>
+                        
                       </div>
-                      <canvas id="audience-chart"></canvas>
+                     
                     </div>
                   </div>
                 </div>
