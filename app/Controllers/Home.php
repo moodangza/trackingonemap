6af00313658103b9c5243ce8636a619d6d06ce8d
moodangza <moodangza29@gmail.com>
2024-01-16 13:19:32 +0700

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -14,7 +14,7 @@ class Home extends BaseController
     public function showprocess(){
         $jobmodel = new jobModel();
         $jobmodel  ->select('job_name ,job_start,job_end,process_name,process_start,process_end,detail,process_status')
-        ->join('process','job.job_id = process.job_id','')
+        ->join('process','job.job_id = process.job_id','LEFT')
         ->orderBy('process_start','desc');
         $process_rs = $jobmodel->findAll();
         $data = [
