7ebef28215b18ab07ea1061ab2e6e596f7b464bf
moodangza <moodangza29@gmail.com>
2024-01-16 10:30:21 +0700

    Update index.php

--- app/Views/spica/index.php
+++ app/Views/spica/index.php
@@ -28,6 +28,7 @@ function drop(ev) {
 }
 </script>
 <script>
+<<<<<<< Updated upstream
   $(document).ready(function () {
     var modal = $("#jobadd");
     $("#intjob").click(function () {
@@ -46,18 +47,27 @@ function drop(ev) {
 });
 $(function(){
             $("#date_start,#date_end").datepicker({
+=======
+ 
+ $("#date_start,#date_end").click(function () {
+            datepicker({
+>>>>>>> Stashed changes
                 language:'th-th',
                 format:'dd/mm/yyyy',
                 autoclose: true
             });
         });
 </script>
+<<<<<<< Updated upstream
 <style>
 #jobadd {
   width: 750px;
   margin: auto;
 }
  </style> 
+=======
+
+>>>>>>> Stashed changes
       <!-- partial -->
       <div class="main-panel">
         <div class="content-wrapper">
@@ -129,6 +139,15 @@ $(function(){
             </div>
            
           </div>
+<<<<<<< Updated upstream
+=======
+          <div id="theModal" class="modal fade text-center">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      </div>
+    </div>
+  </div>
+>>>>>>> Stashed changes
           <div class="row">
             <div class="col-lg-12 grid-margin stretch-card">
               <div class="card">
@@ -353,6 +372,7 @@ $(function(){
     <!-- page-body-wrapper ends -->
   </div>
   <!-- container-scroller -->
+<<<<<<< Updated upstream
   <div class="modal_fade modal-dialog modal-md" id='jobadd' tabindex='-1' role='dialog' >
       <div class="modal-dialog">
         <div class="modal-content">
@@ -386,6 +406,9 @@ $(function(){
       </div>
   </div>
 
+=======
+  
+>>>>>>> Stashed changes
   <?php $this->endSection();?>
 </body>
 
