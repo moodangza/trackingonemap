091d96f955ba060dd3810d796b3c7a194620e422
Patcharapolnuii <127833792+Patcharapolnuii@users.noreply.github.com>
2024-01-16 14:32:09 +0700

    Update index.php

--- app/Views/spica/index.php
+++ app/Views/spica/index.php
@@ -74,11 +74,11 @@ $(function(){
                         <div class="col-md-12">
                           <div class="d-flex justify-content-between traffic-status">
                             <div class="item">
-                              <p class="mb-">TO DO</p>
+                              <p class="mb-">ต้องดำเนินการ</p>
                               <?php for($x=0;$x<3;$x++){?>
                               <div ondrop="drop(event)" ondragover="allowDrop(event)" class="row" style="margin-bottom: 2px">
                               <div class="card" draggable="true" ondragstart="drag(event)">
-                                <h4 class="font-weight-bold mb-0">93,956</h4>
+                                <h4 class="font-weight-bold mb-10">สำนักงานปฏิรูปเพื่อเกษตรกรรม</h4>
                                 <h5>วันที่เริ่ม <?php $date = "04-01-2024";
                                             $date1 = str_replace('-', '/', $date); echo date('d-m-Y',strtotime($date1 . "+$x days"));?></h5>
                                 <h5>วันที่สิ้นสุด</h5>
@@ -88,11 +88,11 @@ $(function(){
                               <div class="color-border"></div>
                             </div>
                             <div class="item">
-                              <p class="mb-">IN PROGRESS</p>
+                              <p class="mb-">กำลังดำเนินการ</p>
                               <?php for($y=0;$y<5;$y++){?>
                               <div ondrop="drop(event)" ondragover="allowDrop(event)" class="row" style="margin-bottom: 2px">
                               <div class="card" draggable="true" ondragstart="drag(event)">
-                                <h4 class="font-weight-bold mb-0">93,956</h4>
+                                <h4 class="font-weight-bold mb-10">กรมอุทยานแห่งชาติ สัตว์ป่า และพันธุ์พืช</h4>
                                 <h5>วันที่เริ่ม <?php $date2 = "06-01-2024";
                                             $date2 = str_replace('-', '/', $date2); echo date('d-m-Y',strtotime($date2 . "+$y days"));?></h5>
                                 <h5>วันที่สิ้นสุด</h5>
@@ -102,11 +102,11 @@ $(function(){
                               <div class="color-border"></div>
                             </div>
                             <div class="item">
-                              <p class="mb-">DONE</p>
+                              <p class="mb-">เสร็จสิ้น</p>
                               <?php for($z=0;$z<5;$z++){?>
                               <div class="row" style="margin-bottom: 2px">
                               <div class="card">
-                                <h4 class="font-weight-bold mb-0">93,956</h4>
+                                <h4 class="font-weight-bold mb-10">กรมป่าไม้</h4>
                                 <h5>วันที่เริ่ม <?php $date3 = "06-01-2024";
                                             $date3 = str_replace('-', '/', $date3); echo date('d-m-Y',strtotime($date3 . "+$z days"));?></h5>
                                 <h5>วันที่สิ้นสุด</h5>
