7675f740bb619c49a201ed922cb83900c28f1bf3
moodangza <moodangza29@gmail.com>
2024-02-08 16:22:23 +0700

    ff

--- app/Controllers/Deletecontroller.php
+++ app/Controllers/Deletecontroller.php
@@ -3,15 +3,18 @@
 namespace App\Controllers;
 use App\Models\deletejobModel;
 //use App\Models\approveModel;
-use App\Models\processModel;
-use App\Models\subprocessModel;
-use App\Controllers\Date;
-use Hermawan\DataTables\DataTable;
+use App\Models\deleteprocessModel;
+use App\Models\deletesubprocessModel;
+
 
 class Manage extends BaseController
 {
     public function deleteprocess($process_id)
     {
-
+        $deleteprocess = new deleteprocessModel();
+        $dataprocess = array('status'=>'2');
+       
+        $deleteprocess ->set($deleteprocess) ->where('process_id',$process_id) -> update();
+       
     }
 }
--- app/Models/deleteprocessmodel.php
+++ app/Models/deleteprocessmodel.php
@@ -17,7 +17,7 @@ class deleteprocessModel extends Model{
     protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
+    // protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
 
     // Validation
