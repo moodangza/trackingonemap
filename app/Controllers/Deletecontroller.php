f79b4817b4169c57c48ef67fa5684908fb0a1446
moodangza <moodangza29@gmail.com>
2024-02-09 10:03:21 +0700

    gg

--- app/Controllers/Deletecontroller.php
+++ app/Controllers/Deletecontroller.php
@@ -12,11 +12,14 @@ class Manage extends BaseController
     public function deleteprocess($process_id)
     {
         $deleteprocess = new processModel();
-        $dataprocess = array('status'=>'2',
+        $dataprocess = array('delete_flag'=>'0',
                               'deleted_at'=>date('Y-m-d H:i:s', strtotime('7 hour'))
                             );
        
-        $deleteprocess ->set($deleteprocess) ->where('process_id',$process_id) -> update();
+        $deleteprocess ->set($dataprocess) ->where('process_id',$process_id) -> update();
+        $deletesubprocess = new subprocessModel();
+        $deletesubprocess ->set($dataprocess) ->where('process_id',$process_id) -> update();
+        
        
     }
 }
