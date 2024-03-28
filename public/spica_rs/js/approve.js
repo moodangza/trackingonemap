a6a4c03b2bffeab61ac3339de4230b17e97c2628
moodangza <moodangza29@gmail.com>
2024-03-28 10:20:10 +0700

    gg

--- public/spica_rs/js/approve.js
+++ public/spica_rs/js/approve.js
@@ -18,21 +18,23 @@ function detailprocessapprove(jobid){
   
     });
     $('#showprocess').html('');
-       
+       const colorlist = ['#80CBC4','#FAD7A0'];
     rs_data.process.forEach(rs_process => {
      
         $('#showprocess').append('<div id="process'+rs_process.process_id+'" '+
-         'class="card" data-bs-toggle="collapse" data-bs-target="#collapseExample'+rs_process.process_id+'" '+ 'aria-expanded="false" aria-controls="collapseExample">'+
+         'class="card" data-bs-toggle="collapse" data-bs-target="#collapsesubprocess'+rs_process.process_id+'" '+ 'aria-expanded="false" aria-controls="collapseExample">'+
         '&nbsp; ขั้นตอนการทำงาน : ' + rs_process.process_name +'<br>&nbsp; วันที่เริ่ม : '+ rs_process.process_start +'&nbsp; วันที่สิ้นสุด : '+ rs_process.process_end +'<br>'+
         '</div>'
         );
         rs_process.subprocess.forEach(rs_subprocess => {
           console.log(rs_subprocess)
-          $('#process'+rs_process.process_id+'').append('<div class="col-1"></div> <div class="col-auto"><div class="list-group list-group-light">'+
+          $('#process'+rs_process.process_id+'').append('<div class="collapse" id="collapsesubprocess'+rs_process.process_id+'"><div class="col-1"></div>'+
+          '<div class="col-auto">'+
+          '<div class="list-group list-group-light" >'+
         
-          '<a href="#" class="list-group-item list-group-item-action px-3 border-0 rounded-3 mb-2 list-group-item-danger">'+
+          '<a href="#" class="list-group-item list-group-item-action px-3 border-0 rounded-3 mb-2 list-group-item-success" style="background-color:'+colorlist+'">'+
             rs_subprocess.subprocess_name+ ' ตั้งแต่วันที่ : '+ rs_subprocess.subprocess_start +' ถึง ' + rs_subprocess.subprocess_end + '</a>'+
-        '</div></div>'
+        '</div></div></div>'
           );
       });
     });
