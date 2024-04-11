cd80bb513e3822dce58d929a173ae4ed4a436872
Patcharapolnuii <165238081+patcharapolonlb@users.noreply.github.com>
2024-04-11 14:11:31 +0700

    6.60

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -10,7 +10,8 @@
           <h4 class="font-weight-bold mb-0 d-none d-md-block mt-1">Welcome back, Brandon Haynes</h4>
           <ul class="navbar-nav navbar-nav-right">
             <li class="nav-item">
-              <h4 class="mb-0 font-weight-bold d-none d-xl-block">Mar 12, 2019 - Apr 10, 2019</h4>
+              <h4 class="mb-0 font-weight-bold d-none d-xl-block">วันที่ <?php date_default_timezone_set('asia/bangkok');
+              $date = date('d-m-Y'); echo $date;?></h4>
             </li>
             <li class="nav-item dropdown mr-1">
               <a class="nav-link count-indicator dropdown-toggle d-flex justify-content-center align-items-center" id="messageDropdown" href="#" data-toggle="dropdown">
