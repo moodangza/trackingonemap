73a1b1618da547c77aab3069f59bbb7930d931d6
moodangza <moodangza29@gmail.com>
2024-05-02 10:16:16 +0700

    h

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -38,7 +38,7 @@
     <script src="<?php echo base_url('spica_rs/vendors/js/vendor.bundle.base.js');?>"></script>
   <!-- endinject -->
   <!-- Plugin js for this page-->
-  <script src="<?php echo base_url('spica_rs/vendors/chart.js/Chart.min.js');?>"></script>
+  <script src="<?php // echo base_url('spica_rs/vendors/chart.js/Chart.min.js');?>"></script>
   <!-- End plugin js for this page-->
   <!-- sweetalert -->
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -65,9 +65,12 @@
     <script type="text/javascript" src="<?php echo base_url('spica_rs/js/locales/bootstrap-datepicker.th.js');?>"></script>
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-modal/2.2.6/js/bootstrap-modalmanager.min.js" integrity="sha512-/HL24m2nmyI2+ccX+dSHphAHqLw60Oj5sK8jf59VWtFWZi9vx7jzoxbZmcBeeTeCUc7z1mTs3LfyXGuBU32t+w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-modal/2.2.6/css/bootstrap-modal.min.css" integrity="sha512-888I2nScRzrb/WNZ3qsa5kiZNmaEsvz8HEVQZRGokIEOj/sMnUlLClqP7itKJYDhXWsmp+1usxoCidSEfW2rWw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
+<link href="<?php echo base_url('spica_rs/bootstrap/css/bootstrap.min.css');?>" rel="stylesheet" >
+<script src="<?php echo base_url('spica_rs/bootstrap/js/bootstrap.bundle.min.js');?>" ></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.7/dist/sweetalert2.all.min.js" ></script>
+<script src="<?php echo base_url('spica_rs/chart.js-4.4.2/chart.js');?>"></script>
 </head>
 <body>
 
